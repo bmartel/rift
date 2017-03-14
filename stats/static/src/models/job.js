a086ef7a35065691f3bb66b47467eb139d359ab0
Brandon Martel <brandonmartel@gmail.com>
2017-03-13 22:37:36 -0500

    initialize ui totals

--- stats/static/src/models/job.js
+++ stats/static/src/models/job.js
@@ -14,34 +14,45 @@ export class Job {
   }
 
   init() {
-    this.apps = prop({});
-    this.error = prop('');
+  //   this.apps = prop({});
+  //   this.error = prop('');
+  //
+  //   // Need to send request to get current state on servers
+  //   const apps = this.apps()[stats.app] || {};
+  //   const totals = apps.totals || {};
+  //   const jobs = apps.jobs || {};
+  //   return {
+  //     totals: {
+  //       ...totals,
+  //       active_jobs: stats.active_jobs || 0,
+  //       queued_jobs: (stats.queued_jobs || 0) + (totals.queued_jobs || 0),
+  //       requeued_jobs: (stats.requeued_jobs || 0) + (totals.requeued_jobs || 0),
+  //       failed_jobs: (stats.failed_jobs || 0) + (totals.failed_jobs || 0),
+  //       processed_jobs: (stats.processed_jobs || 0) + (totals.processed_jobs || 0),
+  //     },
+  //     jobs: {
+  //       ...jobs,
+  //       ...stats.jobs,
+  //     },
+  //   };
+  //   m.redraw();
+  }
 
-    // Need to send request to get current state on servers
-    // const apps = this.apps()[stats.app] || {};
-    // const totals = apps.totals || {};
-    // const jobs = apps.jobs || {};
-    // return {
-    //   totals: {
-    //     ...totals,
-    //     active_jobs: stats.active_jobs || 0,
-    //     queued_jobs: (stats.queued_jobs || 0) + (totals.queued_jobs || 0),
-    //     requeued_jobs: (stats.requeued_jobs || 0) + (totals.requeued_jobs || 0),
-    //     failed_jobs: (stats.failed_jobs || 0) + (totals.failed_jobs || 0),
-    //     processed_jobs: (stats.processed_jobs || 0) + (totals.processed_jobs || 0),
-    //   },
-    //   jobs: {
-    //     ...jobs,
-    //     ...stats.jobs,
-    //   },
-    // };
-    m.redraw();
+  static initialAppTotals(totals) {
+    return totals || {
+      active: 0,
+      queued: 0,
+      processed: 0,
+      failed: 0,
+      requeued: 0,
+    };
   }
 
   updateApps(stats) {
     const apps = this.apps()[stats.app] || {};
-    const totals = apps.totals || {};
+    const totals = Job.initialAppTotals(apps.totals);
     const jobs = apps.jobs || {};
+
     return {
       totals: {
         ...totals,
